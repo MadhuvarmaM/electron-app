c0163fb12f8149f98592749ae1ff0811a383bbda
MadhuvarmaM <madhuvarma4444@gmail.com>
2024-08-16 23:28:02 +0530

    electron setup

---
+++ main.js
@@ -0,0 +1,36 @@
+// import { app, BrowserWindow, IpcMain } from 'electron/main';
+const { app, BrowserWindow,  } = require('electron')
+
+
+const path = require('node:path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+
+
+app.whenReady().then(() => {
+    // ipcMain.handle('ping', () => 'pong')
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
